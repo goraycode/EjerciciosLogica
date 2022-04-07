dec28949836b549599f7449cd4002a975c303a33
AYGJ <jofre1236@gmail.com>
2022-04-06 22:54:08 -0500

    Resolviendo el ejercicio de una forma mas optima y segura

--- js/22-app.js
+++ js/22-app.js
@@ -1,11 +1,14 @@
 /* Ejercicio 
 
 Hacer que el ingreso de una palabra solo me traiga la cadena más larga y sin que contenga letras repetidas
-ejemplo: aaabccc -> abc
+ejemplo: aaabccc -> abc | aurora -> auro
 */
 
 
 //forma 1
+/* De esta forma solo estamos seguros que comparara con el seguro y aparentemente esta bien hasta que realizamos otras comparaciones
+*/
+
 function word(input) {
     let newWord = '';
     for (let i = 0; i < input.length; i++) {
@@ -16,4 +19,15 @@ function word(input) {
     }
 
     return `La nueva palabra es: ${newWord}`;
+}
+
+
+//forma 2
+/* esta es la forma correcta debido a que no permite si o si que se repitan los elementos de un array 
+    y que por más que sean variadas podremos ingresar
+*/
+
+function wordLonger(input) {
+    let mynewWord = [...new Set(input)];
+    return mynewWord.join('');
 }
