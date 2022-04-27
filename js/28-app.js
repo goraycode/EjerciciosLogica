07fac0a9f3d8d305ac9b1b974c5b3844e2f76966
AYGJ <jofre1236@gmail.com>
2022-04-27 17:18:11 -0500

    Separando algoritmo de ordenar de forma ascendente y descendente

--- js/28-app.js
+++ js/28-app.js
@@ -1,18 +1,5 @@
-function getTemperature(temp) {
-    if (temp > 30) {
-        return 'Tropical';
-    } else if (temp > 21 && temp <= 30) {
-        return 'Calor';
-    } else if (temp >= 10 && temp <= 20) {
-        return 'Nublado';
-    } else {
-        return 'Frío';
-    }
-}
-
-function sortNumbers(a, b, c) {
+function sortNumbers(a,b,c) {
     let numbers = [];
-    numbers = [...numbers, a, b, c]
-    return `Ordenado de forma ascendente ${numbers.sort((a, b) => a - b)} y ordernados de forma descendente ${numbers.sort((a, b) => a - b).reverse()}`;
+    numbers = [...numbers,a,b,c];
+    return `Forma ascendente ${numbers.sort((x,y) => x-y)} y forma descente ${numbers.sort((x,y) => x-y).reverse()}`
 }
-
