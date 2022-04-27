66574186c3dee209729ef83adc1c51c5b45546a4
AYGJ <jofre1236@gmail.com>
2022-04-27 18:22:20 -0500

    Realizando el algoritmo de estaciones

--- js/28-app.js
+++ js/28-app.js
@@ -16,3 +16,23 @@ function sortNumbers(a, b, c) {
     numbers = [...numbers, a, b, c];
     return `Forma ascendente ${numbers.sort((x, y) => x - y)} y forma descente ${numbers.sort((x, y) => x - y).reverse()}`
 }
+
+function getSeason(num) {
+    switch (num) {
+        case 1:
+            console.log('verano');
+            break;
+        case 2:
+            console.log('otoño');
+            break;
+        case 3:
+            console.log('Invierno');
+            break;
+        case 4:
+            console.log('primavera');
+            break;
+        default:
+            console.log('No es un ingreso valido');
+            break;
+    }
+}
