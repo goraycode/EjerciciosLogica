43ec9e0a15203ee69fa107ab81da49ff68aad1c8
AYGJ <jofre1236@gmail.com>
2022-04-27 18:18:22 -0500

    Algoritmo de ordenar numeros

--- js/28-app.js
+++ js/28-app.js
@@ -1,5 +1,18 @@
-function sortNumbers(a,b,c) {
+function getTemperature(temp) {
+    if (temp > 30) {
+        return 'Tropical';
+    } else if (temp > 21 && temp <= 30) {
+        return 'Calor';
+    } else if (temp >= 10 && temp <= 20) {
+        return 'Nublado';
+    } else {
+        return 'Frío';
+    }
+}
+
+
+function sortNumbers(a, b, c) {
     let numbers = [];
-    numbers = [...numbers,a,b,c];
-    return `Forma ascendente ${numbers.sort((x,y) => x-y)} y forma descente ${numbers.sort((x,y) => x-y).reverse()}`
+    numbers = [...numbers, a, b, c];
+    return `Forma ascendente ${numbers.sort((x, y) => x - y)} y forma descente ${numbers.sort((x, y) => x - y).reverse()}`
 }
